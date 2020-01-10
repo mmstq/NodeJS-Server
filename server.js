a17d9bf81fc87c71388e2636a67ea6e2bc8bc3bb
mmstq <mushtakkhan9@gmail.com>
2020-01-10 22:41:37 +0530

    new start

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -55,6 +56,6 @@ app.get('/', (req, res) => {
 
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 });
