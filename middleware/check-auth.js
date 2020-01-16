ae0b17f558a4e5387478b2dbe00ff88bcb950d24
mmstq <mushtakkhan9@gmail.com>
2020-01-16 19:31:56 +0530

    simplified error messages

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,24 +1,16 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-    try {
+module.exports = (req, res, next)=>{
+    try{
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, '@qwerty312', (err, decoded) => {
-            if (err) {
-                return res.status(404).json({
-                    message: err.message || "Token expired"
-                });
-            } else {
-                req.userData = decoded;
-                next();
-            }
-
-        });
-
-    } catch (error) {
+        const decoded = jwt.verify(token, '@qwerty312');
+        req.userData = decoded;
+        next();
+    }catch(error){
         res.status(401).json({
-            message: error.message || 'Authorization required for this action'
+            message: error ||'Authorization required for this action'
         });
+        console.log(error);
     }
-
+    
 };
