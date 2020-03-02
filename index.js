e9a2bc2d488240b8f13aaaeb74c4106f38a34e7f
mmstq <mushtakkhan9@gmail.com>
2020-03-02 19:57:31 +0530

    added search feature

--- index.js
+++ index.js
@@ -100,6 +100,30 @@ socketIO.on('connection', (socket) => {
             });
     });
 });
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'mmstq.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
 // listen for requests
 http.listen(PORT, () => {
     console.log("Server is listening on port: " + PORT);
