580a3bfde96e4e2b1f0f1f470caa0f72d37b1911
mmstq <mushtakkhan9@gmail.com>
2020-03-16 16:33:38 +0530

    login using username

--- app/controller/note.controller.js
+++ app/controller/note.controller.js
@@ -33,7 +33,7 @@ exports.create = async (req, res) => {
 };
 
 exports.findAll = async (req, res) => {
-    await Note.find().sort('-__v')
+    await Note.find().sort('-time')
         .then(notes => {
             res.send(notes);
             console.log(notes)
