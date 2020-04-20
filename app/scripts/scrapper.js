d4e434e3204f1d4fe063385819c303226ba74755
mmstq <mushtakkhan9@gmail.com>
2020-04-20 17:25:07 +0530

    added 2 python scripts

--- app/scripts/scrapper.js
+++ app/scripts/scrapper.js
@@ -1,29 +1,24 @@
-// const webdriver = require('selenium-webdriver');
-const express = require('express')
-const router = express.Router()
-var admin = require("firebase-admin")
-var serviceAccount = require("../scripts/service.json")
+const admin = require("firebase-admin")
+const serviceAccount = require("../scripts/service.json")
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://uiet-mdu-bc58d.firebaseio.com"
 })
+const sendMsg = function sendNotification(title, body, link) {
 
+    const registrationToken = 'fOMqeAI7L20:APA91bEYtnY-mshByqGEWIOwJW78vGHOW8C2oQSVAshXfy6ypscpE_msdjO1ETE1ge3o9po2Jv3HDPs2BdqMSQCs2QNNwM1ItIWs0bulJKIDt1tYeCMpWcBO_0i1I03Ou7owtkaRmJr6'
 
-// const By = webdriver.By;
-
-router.get('/test', function (req, res) {
-
-
-    var registrationToken = 'fOMqeAI7L20:APA91bEYtnY-mshByqGEWIOwJW78vGHOW8C2oQSVAshXfy6ypscpE_msdjO1ETE1ge3o9po2Jv3HDPs2BdqMSQCs2QNNwM1ItIWs0bulJKIDt1tYeCMpWcBO_0i1I03Ou7owtkaRmJr6'
-
-    var payload = {
+    const payload = {
         notification: {
-            title: "Account Deposit",
-            body: "A deposit to your savings account has just cleared."
+            title: title,
+            body: body
+        },
+        data: {
+            link: link
         }
     };
 
-    var options = {
+    const options = {
         priority: "high",
         timeToLive: 60 * 60 * 24
     };
@@ -36,23 +31,6 @@ router.get('/test', function (req, res) {
             console.log("Error sending message:", error);
         });
 
-    // const driver = new webdriver.Builder()
-    //     .forBrowser('firefox')
-    //     .build();
-    // driver.get('http://www.google.com/ncr');
-    // driver.findElement(By.name('q')).sendKeys('webdriver');
-    // driver.findElement(By.name('btnG')).click();
-    // driver.wait(function() {
-    //     return driver.getTitle().then(function(title) {
-    //         console.log(title);
-    //         return title === 'webdriver - Google Search';
-    //     });
-    // }, 5000).then(function() {
-    //     res.status(200).send('Done');
-    // }, function(error) {
-    //     res.status(200).send(error);
-    // });
-    // driver.quit();
-});
+}
 
-module.exports = router
+module.exports = sendMsg
